69202ce6b3051fac1abfe95da84085b0241e558b
Arif Asfe <arifasfe@gmail.com>
2024-02-01 23:30:10 +0600

    added render file

--- api/CuetMedicalAPI/CuetMedicalAPI/settings.py
+++ api/CuetMedicalAPI/CuetMedicalAPI/settings.py
@@ -33,9 +33,9 @@ import cloudinary.api
 SECRET_KEY = "django-insecure-)ve*s3#@$$!7e63-%xr_dj_i5@w5vvntv(tdib&liax-)u90cx"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cuetmedicalcenter.onrender.com']
 
 
 # Application definition
@@ -133,6 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'ui/cuetmedicalui/build/static',
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
