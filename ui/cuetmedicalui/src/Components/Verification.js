d98d1ca44645fb9751f41bf0b8c2604aed3c32d1
Mohammad Akbar <mohammad.akbar2603@gmail.com>
2024-03-18 10:41:51 +0600

    Fixed .catch and edited buttons

--- ui/cuetmedicalui/src/Components/Verification.js
+++ ui/cuetmedicalui/src/Components/Verification.js
@@ -40,8 +40,11 @@ function Verification({ students, setStudents }) {
       .then(response => {
         setStudents(students.filter(student => student.id !== studentId));
 
+        toggle();
       })
-
+      .catch(error => {
+        console.error('Failed to delete student:', error);
+      });
   };
 
   const handleSeeDetails = (student) => {
@@ -105,7 +108,7 @@ function Verification({ students, setStudents }) {
           <Button color="danger" onClick={() => handleDelete(selectedStudent.id)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
           </svg></Button>{' '}
-          <Button color="secondary" onClick={toggle}>Cancel</Button>
+          {/* <Button color="secondary" onClick={toggle}>Cancel</Button> */}
         </ModalFooter>
       </Modal>
     </div>
